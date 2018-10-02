9bf44e15d63ffd5e527bbe1089734eb6bdef7fd7
TheUser <facuj@travala10.com>
2018-10-02 18:43:48 +0300

    all commited

---
+++ EntranceLogger/CreationVisitorWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EntranceLogger
+{
+    public partial class CreationVisitorWindow : Window
+    {
+        private static readonly Regex numericRegex = new Regex("[0-9]+");
+        private EntranceLoggerEntities source;
+
+        public CreationVisitorWindow(EntranceLoggerEntities dataSource)
+        {
+            InitializeComponent();
+            DataContext = new CreationVisitorViewModel(dataSource) { CloseAction = OnClose };
+            source = dataSource;
+        }
+
+        private static bool IsNumericAllowed(string text)
+        {
+            return numericRegex.IsMatch(text);
+        }
+
+        private void NumericPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsNumericAllowed(e.Text);
+        }
+
+        private void OnClose()
+        {
+            DialogResult = true;
+            Close();
+        }
+    }
+}
---
+++ EntranceLogger/EmployeeViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntranceLogger
+{
+    public class EmployeeViewModel
+    {
+        public Employee Employee { get; set; }
+        public string Fullname { get {
+                return Employee.Surname + " " + Employee.Name + " " + Employee.Patronymic ?? ""; } }
+
+        public EmployeeViewModel(Employee employee)
+        {
+            Employee = employee;
+        }
+
+    }
+}
